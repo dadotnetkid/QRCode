13f24c621e6e89f8079c30e9892b4f4744055820
dadotnetkid <markchristopher.cacal@gmail.com>
2020-08-31 10:55:28 +0800

    Added Profile Page

---
+++ QRCode/QRCode/Views/ProfilePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using QRCode.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing;
+using ZXing.Net.Mobile.Forms;
+using ZXing.QrCode;
+
+namespace QRCode.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        void Init()
+        {
+            ContainerLayout.Children.Add(
+            new QRCodeGeneratorService().GenerateQR(
+                "ewogICJGaXJzdE5hbWUiOiJNYXJrIENocmlzdG9waGVyIiwKICAiTGFzdE5hbWUiOiJDYWNhbCIsCn0="));
+        }
+    }
+}
