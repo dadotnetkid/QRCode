9de5b3e48b0e0c1b5328f73de0dda420f8383c2a
dadotnetkid <markchristopher.cacal@gmail.com>
2020-09-01 14:43:52 +0800

    Added registration Method

--- QRCode/QRCode/Views/RegistrationPage.xaml.cs
+++ QRCode/QRCode/Views/RegistrationPage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using QRCode.ViewModels;
+using RestSharp;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -21,6 +23,27 @@ namespace QRCode.Views
         {
             Application.Current.MainPage = new LoginPage();
             return true;
+        }
+
+        private async void btnRegister(object sender, EventArgs e)
+        {
+            try
+            {
+                RestClient restClient = new RestClient("http://");
+                RestRequest restRequest = new RestRequest();
+                restRequest.AddJsonBody(new
+                {
+                    Email = txtEmail.Text,
+                    FirstName = txtFirstName.Text,
+                    LastName = txtLastName.Text,
+                    Password = txtPassword.Text
+                });
+                await restClient.ExecuteAsync(restRequest);
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception.Message);
+            }
         }
     }
 }
